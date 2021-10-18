7e0a19334d95e52e88babde4bd5dfac22244695d
Tiago Vinicius <titiovini85@gmail.com>
2021-10-18 00:58:36 -0300

    mudancas no prunning

--- eda/trabalhos/minimax/src/minimax.py
+++ eda/trabalhos/minimax/src/minimax.py
@@ -1,6 +1,5 @@
 from game import Game, TicTacToe
 from components import StateNode, Player
-from typing import List
 from math import inf
 from timeit import default_timer as timer
 from datetime import timedelta
@@ -33,7 +32,7 @@ class MiniMax:
         elif turn == Player.PLAYER_1:
             best_value = -inf
             pos = state.position
-
+            prune_here = False
             for x in range(3):
                 for y in range(3):
                     if pos[x][y] == "-":
@@ -46,18 +45,20 @@ class MiniMax:
                                                                    beta=beta))
                         pos[x][y] = "-"
                         best_value -= node_depth
-
+                        alpha = max(alpha, best_value)
                         if self.prune:
-                            alpha = max(alpha, best_value)
                             if beta <= alpha:
-                                break
+                                prune_here = True
+
+                if prune_here:
+                    break
 
             return best_value
 
         else:
             best_value = +inf
             pos = state.position
-
+            prune_here = False
             for x in range(3):
                 for y in range(3):
                     if pos[x][y] == "-":
@@ -71,16 +72,16 @@ class MiniMax:
                         pos[x][y] = "-"
                         best_value += node_depth
 
+                        beta = min(beta, best_value)
                         if self.prune:
-                            beta = min(beta, best_value)
                             if beta <= alpha:
-                                break
+                                prune_here = True
+                if prune_here:
+                    break
             return best_value
 
     def best_move(self, state: StateNode, player: Player, alpha=None, beta=None) -> ((int, int), int):
         best_val = -inf
-        best_move = (-1, -1)
-
         board = state.position
         for i in range(3):
             for j in range(3):
@@ -94,51 +95,57 @@ class MiniMax:
                     board[i][j] = "-"
 
                     if val > best_val:
-                        best_move = (i, j)
                         best_val = val
-        return best_move, best_val
+        return best_val
 
     @staticmethod
     def winner(value: float) -> None:
-        match_winner = "EMPATE"
-        if value == +1:
+        match_winner = "INDEFINIDO"
+        if value > 0:
             match_winner = Player.PLAYER_1.value
-        elif value == -1:
+        elif value < 0:
             match_winner = Player.PLAYER_2.value
-        print(f"Valor final: {value}. O vencedor é {match_winner}")
+        elif value == 0:
+            match_winner = "EMPATE"
+        print(f"O vencedor é {match_winner}")
+        print(f"Melhor avaliação: {value}")
 
     def compute_stats(self, state: StateNode) -> None:
+        print()
         start = timer()
-        move, val = self.best_move(state, Player.PLAYER_1, -inf, inf)
+        val = self.best_move(state, Player.PLAYER_1, -inf, inf)
         self.winner(val)
         print(f"Maior profundidade: {self.game.max_depth}")
-        print(f"Melhor movimento: {move}")
+        print(f"Poda Alfa-Beta: {self.prune}")
         end = timer()
         print(timedelta(seconds=end-start))
+        print()
 
 
 if __name__ == '__main__':
 
-    # board = [
-    #     ['X', 'O', 'X'],
-    #     ['O', 'O', 'X'],
-    #     ['-', '-', '-']
-    # ]
-
     board = [
-        ['-', '-', '-'],
-        ['-', '-', '-'],
+        ['X', 'O', 'X'],
+        ['X', 'O', 'O'],
         ['-', '-', '-']
     ]
+
+    # board = [
+    #     ['-', '-', '-'],
+    #     ['-', '-', '-'],
+    #     ['-', '-', '-']
+    # ]
 
     s: StateNode = StateNode(board, -inf, 0)
     tic_tac_toe = TicTacToe(s)
 
     mm1 = MiniMax(tic_tac_toe, prune=True)
     mm1.compute_stats(s)
+    mm1.game.show_moves()
 
     mm2 = MiniMax(tic_tac_toe, prune=False)
     mm2.compute_stats(s)
+    mm2.game.show_moves()
 
 
 
